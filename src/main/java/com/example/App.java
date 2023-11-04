436873ce292b3e55fd830d594330db5366f43347
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2023-11-04 12:51:37 -0400

    add zip file input

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,11 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!!!! :)))))" );
+        ZipFileInput z = new ZipFileInput("Enter zip file path here");
+        z.readFiles();
     }
 }
