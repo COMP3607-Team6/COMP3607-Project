e9f286a39c728fbd4e3da04bd4b8184e400113dd
kimberly-moses <kimberly0moses@gmail.com>
2023-11-06 23:40:23 -0400

    gui started

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!!!! :)))))" );
-        ZipFileInput z = new ZipFileInput("Enter zip file path here");
-        z.readFiles();
+        // ZipFileInput z = new ZipFileInput("Enter zip file path here");
+        // z.readFiles();
+
+        AssignmentSpecPortal assignmentSpecPortal = new AssignmentSpecPortal();
+        assignmentSpecPortal.setVisible(true);
     }
 }
